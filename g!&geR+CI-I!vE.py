e45a8ed88075b518b5d88c36dd25b293f5bb3a31
Yue Ying <yueying@Yues-MacBook-Pro.local>
2020-04-15 18:16:54 -0500

    finished code poetry: Everyday is Valentine

---
+++ g!&geR+CI-I!vE.py
@@ -0,0 +1,34 @@
+
+# ---------------------------------------------
+# CODE.POETRY - Everyday is Valentine
+#
+# (c) 2020 ../che*r!o CHICAGO, IL
+# e_mAIl cherry.yue.ying@gmail.com
+# ---------------------------------------------
+
+I_o\/e = open("I_0v_E.txt")
+
+g!ngeR = {
+    'co[lo]r': 'RED',
+    'duration': 'fore[ve]r',
+    '[love]Target': 'CH!vE'
+}
+CH!vE = {
+    'cOlOr': 'rose',
+    'durAt{i}on': 365,
+    'LOVE->>>': 'g!ngeR'
+}
+
+def once_U_pon_AT_ime():
+    if g!ngeR.getDistanceTo(CH!vE) < 20170:
+        g!ngeR['FeeL<ing>$'] = 'Yin.I_ove'
+        if CH!vE.getFeelings(g!ngeR):
+            print(I_o\/e)
+        else:
+            print('(*O w O)')
+    else:
+        open("c_h[g]a(o)_sy(i)ng.txt")
+
+I_o\/e.close()
+
+# ../che*r!o 2020
